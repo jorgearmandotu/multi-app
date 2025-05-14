18fceb335b7020a9ba96b56f2509225033bf7cce
Jorge Urbina <jorgearmandou@gmail.com>
2025-05-14 18:02:59 -0500

    instaladion de env

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,31 @@
 
+import 'package:http/http.dart' as http;
 
 class AuthService {
   // This class will handle authentication-related tasks
   // such as login, logout, and user session management.
 
+  bool _authProcess = false;
+
+  set authProcess(bool value) {
+    _authProcess = value;
+  }
   // Example method for user login
-  Future<bool> login(String username, String password) async {
+  Future<bool> login(String email, String password) async {
     // Implement your login logic here
+    authProcess = true;
+
+    final data = {
+      'email': email,
+      'password': password,
+    };
+
+    final resp = await http.post(Uri.parse('${ Environment.apiUrl }/login'),
+    body: jsonEncode(data),
+    headers: {
+      'Content-Type': 'application/json'
+    });
+
     return true; // Return true if login is successful
   }
 

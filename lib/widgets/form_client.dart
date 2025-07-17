6291d3c5db9109989ad5439a50041629f4b88df3
unknown <jorgearmandou@gmail.com>
2025-07-16 20:05:18 -0500

    se realizan cambios en diseño formulario cliente

--- lib/widgets/form_client.dart
+++ lib/widgets/form_client.dart
@@ -9,21 +9,44 @@ class FormCLient extends StatefulWidget {
 }
 
 
-enum ClientType { nit(), cedula(), cedulaExtranjeria() }
 
 
+List<String> clientType = ['CC', 'NIT', 'CE'];
 
 class _FormCLientState extends State<FormCLient> {
-
+   
+   String dropdownValue = clientType.first;
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
+        spacing: 20,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
         children: [
-          DropdownMenu<ClientType>(
-            dropdownMenuEntries: ClientType.values.map((e) => DropdownMenuEntry<ClientType>(value: e, label: e.name)).toList(),
-            initialSelection: ClientType.nit,
-            label: const Text('Tipo de Identificación del Cliente'),
+          Text('Creacion de Clientes',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          //SizedBox(height: 20,),
+          SizedBox(
+            //width: double.infinity,
+            child: DropdownMenu<String>(
+              width: double.infinity,
+              leadingIcon: const Icon(Icons.tab),
+              dropdownMenuEntries: clientType.map<DropdownMenuEntry<String>>((String value) {
+                return DropdownMenuEntry<String>(
+                  value: value,
+                  label: value,
+                );
+              }).toList(),
+              initialSelection: clientType.first,
+              label: const Text('Tipo de Identificación'),
+              onSelected: (String? value) {
+                setState(() {
+                  dropdownValue = value ?? clientType.first;
+                });
+              },
+            ),
           )
         ]
       )
